685ced6cd51266422a37d2e5333b0b79fdc2db36
stosdenfer <fran@markulin.com>
2021-10-27 22:29:45 +0200

    Create zad10.cpp
    nez kak da to bude O(k) :shrug:

---
+++ lab1/zad10.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+int factorial(int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+
+    return factorial(n - 1) * n;
+}
+
+double f (double z, int k)
+{
+    if(k < 0)
+        return 0;
+
+    return f(z, k - 1) + (pow(-1, k) * pow(z, (2 * k) + 1)) / factorial((2 * k) + 1);
+}
+
+int main(void)
+{
+    double z = 0.5;
+    while(true)
+    {
+        int k;
+        std::cout << "Upišite željenu vrijednost za k ili broj 42069 za prekid rada programa: ";
+        std::cin >> k;
+        
+        if(k == 42069)
+        {
+            break;
+        }
+
+        if(k < 0)
+        {
+            std::cout << "Molim upišite broj >= 0!";
+            continue;
+        }
+
+        std::cout << "f = " << f(z, k) << std::endl; 
+    }
+    
+    return 0;
+}
