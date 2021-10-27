6d54853077e1be13cc57ca330ba8696ce6d667cc
stosdenfer <fran@markulin.com>
2021-10-27 21:38:36 +0200

    added zad8.cpp

---
+++ lab1/zad8.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+class SanitizedString
+{
+private:
+    std::string str;
+public:
+    SanitizedString(std::string ulaz)
+    {
+        str = ulaz;
+        removeDuplicateWhitespace();
+        removeNonAlphaChars();
+    }
+
+    void removeDuplicateWhitespace()
+    {
+        for(int i = 1; i < str.size(); i++)
+        {
+            if(str[i] == ' ' && str[i - 1] == ' ')
+            {
+                str.erase(str.begin() + i);
+                i--;
+            }
+        }
+    }
+
+    void removeNonAlphaChars()
+    {
+        for(int i = 0; i < str.size(); i++)
+        {
+            if(!(isalpha(str[i]) || str[i] == ' '))
+            {
+                str.erase(str.begin() + i);
+                i--;
+            }
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const SanitizedString& str);
+};
+
+std::ostream& operator<<(std::ostream& os, const SanitizedString& str)
+{
+    os << str.str;
+    return os;
+}
+
+int main(void)
+{
+    std::string ulaz;
+    std::cout << "Upišite znakovni niz: ";
+    std::getline(std::cin, ulaz);
+
+    SanitizedString sanitized(ulaz);
+
+    std::cout << "Početni string: " << ulaz << std::endl;
+    std::cout << "Sanitizirani string: " << sanitized << std::endl;
+
+    return 0;
+}
