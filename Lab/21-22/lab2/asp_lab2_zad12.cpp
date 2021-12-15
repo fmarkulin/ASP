9264192268bbf6d96123d589aed3c190537572b2
stosdenfer <fran@markulin.com>
2021-12-15 15:57:44 +0100

    reši 12.

--- Lab/21-22/lab2/asp_lab2_zad12.cpp
+++ Lab/21-22/lab2/asp_lab2_zad12.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <string>
 
 class Osoba
 {
 private:
     std::string ime;
     unsigned short int starost;
+    friend bool operator<(Osoba const &a, Osoba const &b);
 
 public:
+    Osoba() {};
     Osoba(std::string ime, unsigned short int starost) : ime(ime), starost(starost) {}
     std::string getIme() { return ime; }
     unsigned short int getStarost() { return starost; }
@@ -14,7 +17,6 @@ public:
     void setStarost(unsigned short int starost) { this->starost = starost; }
     ~Osoba() { std::cout << "Obrisan/a " << this->ime << "|" << this->starost << std::endl; }
 
-    friend bool operator<(Osoba const &a, Osoba const &b);
     Osoba &operator=(const Osoba &osoba)
     {
         this->ime = osoba.ime;
@@ -34,12 +36,46 @@ bool operator<(Osoba const &a, Osoba const &b)
 template <typename T>
 void InsertionSort(T A[], int n)
 {
-    T temp;
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i < n; i++)
     {
-        temp = A[i];
+        T temp = A[i];
         int j;
-        for(j = i + 1; j >= 1 && A[j - 1] > A[j]; j--)
-            A[j] = A[j - 1];   
+        for(j = i; j >= 1 && temp < A[j - 1]; j--)
+            A[j] = A[j - 1];
+        A[j] = temp;
     }
+}
+
+int main(void)
+{
+    static const int MAX = 5;
+    Osoba *osobe = new Osoba[MAX];
+    for(int i = 0; i < MAX; i++)
+    {
+        std::string ime;
+        std::cout << "Upišite ime " << i + 1 << ". osobe: ";
+        std::getline(std::cin, ime);
+        unsigned short int starost;
+        std::cout << "Upisite starost " << i + 1 << ". osobe: ";
+        std::cin >> starost;
+        std::cin.ignore(1, '\n');
+        osobe[i] = Osoba(ime, starost);
+    }
+
+    InsertionSort(osobe, MAX);
+
+    std::cout << "{";
+    for(int i = 0; i < MAX; i++)
+    {
+        if(i == MAX - 1)
+        {
+            std::cout << "{\"" << osobe[i].getIme() << "\"," << osobe[i].getStarost() << "}}" << std::endl;
+            break;
+        }
+        std::cout << "{\"" << osobe[i].getIme() << "\"," << osobe[i].getStarost() << "},";
+    }
+
+    delete[] osobe;
+
+    return 0;
 }
