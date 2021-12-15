456dc6a1591a996e98bf1080b732cab85ae859fc
stosdenfer <fran@markulin.com>
2021-12-15 17:22:17 +0100

    fix zad10, nisam radio obostrano

--- Lab/21-22/lab2/asp_lab2_zad10.cpp
+++ Lab/21-22/lab2/asp_lab2_zad10.cpp
@@ -5,33 +5,47 @@ void selection2Sort(int A[], int n, char smjer)
     if (smjer == '0')
     { // uzlazno sortiranje
         int i, j;
-        for (i = 0; i < n; i++)
+        for (i = 0; i < n; i++, n--)
         {
             int min = i;
+            int max = n - 1;
             for (j = i + 1; j < n; j++)
             {
                 if (A[j] < A[min])
                     min = j;
+                if(A[j] > A[max])
+                    max = j;
             }
             int temp = A[i];
             A[i] = A[min];
             A[min] = temp;
+
+            temp = A[n - 1];
+            A[n - 1] = A[max];
+            A[max] = temp;
         }
     }
     else
     {
         int i, j;
-        for (i = 0; i < n; i++)
+        for (i = 0; i < n; i++, n--)
         {
             int max = i;
+            int min = n - 1;
             for (j = i + 1; j < n; j++)
             {
                 if (A[j] > A[max])
                     max = j;
+                if(A[j] < A[min])
+                    min = j;
             }
             int temp = A[i];
             A[i] = A[max];
             A[max] = temp;
+
+            temp = A[n - 1];
+            A[n - 1] = A[min];
+            A[min] = temp;
         }
     }
 }
