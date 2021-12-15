ea28639fdb98f0c5f26d62cc511bb006a9e1c0c0
stosdenfer <fran@markulin.com>
2021-12-15 23:01:09 +0100

    fix selection2sort
    problem su bile dvije stvari
    nisam mogo radit samo s n, nego sam morao uzeti pomoću varijablu k = n - 1, kako bih na početku mogao imati i < k (i < n - 1) i onda dalje smanjivati za jedan
    
    da sam stavio i < n - 1, i onda posilje n--, radio bi u idućim koracima duplu dekrementaciju i izlazio bih iz polja
    a na početku mora ići do n - 1 jer u protivnom usporedi jedan element previše
    
    drugi problem je bio taj što nakon prvog swap-a ne provjeravam jesam li tim swapom utjecao na idući
    npr, ako sam swapao min, na novo swapano mjesto je sada možda došla ta nađena max vrijednost, i onda bi ju krivo prebacio bez provjere

--- Lab/21-22/lab2/asp_lab2_zad10.cpp
+++ Lab/21-22/lab2/asp_lab2_zad10.cpp
@@ -4,48 +4,70 @@ void selection2Sort(int A[], int n, char smjer)
 {
     if (smjer == '0')
     { // uzlazno sortiranje
-        int i, j;
-        for (i = 0; i < n; i++, n--)
+        int i, j, k;
+        for (i = 0, k = n - 1; i < k; i++, k--)
         {
             int min = i;
             int max = n - 1;
-            for (j = i + 1; j < n; j++)
+            for (j = i + 1; j <= k; j++)
             {
                 if (A[j] < A[min])
                     min = j;
-                if(A[j] > A[max])
+                if (A[j] > A[max])
                     max = j;
             }
+
+            int maxv = A[max];
             int temp = A[i];
             A[i] = A[min];
             A[min] = temp;
 
-            temp = A[n - 1];
-            A[n - 1] = A[max];
-            A[max] = temp;
+            if (A[min] == maxv)
+            {
+                temp = A[n - 1];
+                A[n - 1] = A[min];
+                A[min] = temp;
+            }
+            else
+            {
+                temp = A[n - 1];
+                A[n - 1] = A[max];
+                A[max] = temp;
+            }
         }
     }
     else
     {
-        int i, j;
-        for (i = 0; i < n; i++, n--)
+        int i, j, k;
+        for (i = 0, k = n - 1; i < k; i++, k--)
         {
             int max = i;
             int min = n - 1;
-            for (j = i + 1; j < n; j++)
+            for (j = i + 1; j <= k; j++)
             {
                 if (A[j] > A[max])
                     max = j;
-                if(A[j] < A[min])
+                if (A[j] < A[min])
                     min = j;
             }
+            int minv = A[min];
+
             int temp = A[i];
             A[i] = A[max];
             A[max] = temp;
 
-            temp = A[n - 1];
-            A[n - 1] = A[min];
-            A[min] = temp;
+            if (A[max] == minv)
+            {
+                temp = A[n - 1];
+                A[n - 1] = A[max];
+                A[max] = temp;
+            }
+            else
+            {
+                temp = A[n - 1];
+                A[n - 1] = A[min];
+                A[min] = temp;
+            }
         }
     }
 }
@@ -55,9 +77,9 @@ int main(void)
     int n;
     std::cout << "Upisite broj clanova polja: ";
     std::cin >> n;
-    
+
     int *A = new int[n];
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         std::cout << "Upisite " << i << ". element polja: ";
         std::cin >> A[i];
@@ -69,7 +91,7 @@ int main(void)
 
     selection2Sort(A, n, smjer);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         std::cout << A[i] << " ";
     std::cout << std::endl;
 
